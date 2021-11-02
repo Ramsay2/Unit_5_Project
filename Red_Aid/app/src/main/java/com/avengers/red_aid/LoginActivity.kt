f2c831fc98ad134fe49f684dd51cb53464e86741
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-11-02 15:16:36 +0530

    initial commit.

---
+++ Red_Aid/app/src/main/java/com/avengers/red_aid/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.avengers.red_aid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toolbar
+import androidx.core.graphics.drawable.toDrawable
+import com.google.android.material.appbar.MaterialToolbar
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ Red_Aid/app/src/main/java/com/avengers/red_aid/LoginFragment.kt
@@ -0,0 +1,32 @@
+package com.avengers.red_aid
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+class LoginFragment() : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = Navigation.findNavController(view)
+        val mTvGetOTP = view.findViewById<Button>(R.id.btnOTP)
+        mTvGetOTP.setOnClickListener {
+            navController.navigate(R.id.action_loginFragment_to_otpFragment)
+        }
+    }
+
+}
