a9eb21e9fd93f14e600b87186a2cc87c24000ad5
kunalcodes <kunal.chaturvedi75@gmail.com>
2021-11-12 17:52:54 +0530

    PaymentFragment.kt added.

--- Red_Aid/app/src/main/java/com/avengers/red_aid/views/PaymentFragment.kt
+++ Red_Aid/app/src/main/java/com/avengers/red_aid/views/PaymentFragment.kt
@@ -1,34 +1,29 @@
 package com.avengers.red_aid.views
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import com.avengers.red_aid.R
+import com.avengers.red_aid.databinding.FragmentCheckEligibilityBinding
+import com.avengers.red_aid.databinding.FragmentPaymentBinding
 
-class HomeFragment : Fragment() {
 
-    var mtvText :ImageView? = null
+class PaymentFragment : Fragment() {
+    private lateinit var paymentFragment: FragmentPaymentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        paymentFragment = FragmentPaymentBinding.inflate(layoutInflater)
+
+        return paymentFragment.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mtvText = view.findViewById(R.id.tvText)
-        mtvText?.setOnClickListener {
-            val intent = Intent(context, CheckEligibilityActivity::class.java)
-            startActivity(intent)
-        }
 
     }
-
 }
